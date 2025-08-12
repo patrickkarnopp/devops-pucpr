1202b5a384b1330c6194b03ceab302651382611c
Patrick Karnopp Robetti <patrick_robetti@hotmail.com>
2025-08-12 16:18:33 -0300

    Config inicial
    Configuração inicial de um API usando o FASTAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/teste1")
+async def teste1():
+    return {"message": "Hello World from teste1"}
