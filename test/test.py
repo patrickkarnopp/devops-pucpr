608ca6dd499fbec2a69d92f4cac0ffbec17a1c8d
Patrick Karnopp Robetti <patrick_robetti@hotmail.com>
2025-09-15 20:52:11 -0300

    Refactor tests to use asyncio and await for asynchronous function calls

--- test/test.py
+++ test/test.py
@@ -1,32 +1,40 @@
+import pytest
 from src.main import *
 from unittest.mock import patch
 
-def test_root():
-    result = root()
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
     assert result == {"message": "Hello World"}
 
-def test_funcaoteste():
+@pytest.mark.asyncio
+async def test_funcaoteste():
     with patch('random.randint', return_value=12345):
-        result = funcaoteste()
+        result = await funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
-def test_create_estudante():
+@pytest.mark.asyncio
+async def test_create_estudante():
     estudante_test = Estudante(name="Pedro", curso="Engenharia", ativo=False)
-    result = create_estudante(estudante_test)
+    result = await create_estudante(estudante_test)
     assert estudante_test == result
 
-def test_update_estudante_negativo():
-    result = update_estudante(-5)
+@pytest.mark.asyncio
+async def test_update_estudante_negativo():
+    result = await update_estudante(-5)
     assert not result
 
-def test_update_estudante_positivo():
-    result = update_estudante(10)
+@pytest.mark.asyncio
+async def test_update_estudante_positivo():
+    result = await update_estudante(10)
     assert result
 
-def test_delete_estudante_negativo():
-    result = delete_estudante(-5)
+@pytest.mark.asyncio
+async def test_delete_estudante_negativo():
+    result = await delete_estudante(-5)
     assert not result
 
-def test_delete_estudante_positivo():
-    result = delete_estudante(10)
+@pytest.mark.asyncio
+async def test_delete_estudante_positivo():
+    result = await delete_estudante(10)
     assert result
