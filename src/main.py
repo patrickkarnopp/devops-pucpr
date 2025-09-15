bdf60dcff8b9d3b6643068e64d0bf712e7bd0411
Patrick Karnopp Robetti <patrick_robetti@hotmail.com>
2025-09-15 20:07:43 -0300

    Add FastAPI application with student management endpoints

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import random
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Estudante(BaseModel):
+    nome: str
+    curso: str
+    ativo: bool
+
+@app.get("/helloworld")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/funcaoteste")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
+
+@app.post("/estudante/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.get("/estudante/update/{id_estudante}")
+async def update_estudante(id_estudante: int):
+    return id_estudante > 0
+
+@app.delete("/estudante/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
